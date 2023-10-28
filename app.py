221d9a2907df83319228413639a4d3fe2ebd39c1
Saad Ibrahim <isaad3.one@gmail.com>
2023-10-28 18:28:05 -0400

    flask_session-exercise

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -11,28 +11,38 @@ debug = DebugToolbarExtension(app)
 
 
 responses = []
-
+RESPONSES_KEY = "responses"
 
 @app.route("/", methods=['GET', 'POST'])
 def start_of_survey():
   return render_template('survey_start.html', survey=survey)
 
+#----------------------------------------------------------------------------------------------
+
 @app.route('/questions/<int:qid>', methods=['GET', 'POST'])
 def question(qid):
+
+  session[RESPONSES_KEY] = []
+
+  responses = session.get(RESPONSES_KEY)
 
   if (len(responses) != qid):
         flash(f"Invalid question")
         return redirect(f"/questions/{len(responses)}")
   
-
   return render_template('question_0.html', question=survey.questions[qid])
+
+#----------------------------------------------------------------------------------------------
 
 @app.route("/answer", methods=['GET', 'POST'])
 def answer():
   choice = request.form['answer']
 
+  responses = session[RESPONSES_KEY]
   responses.append(choice)
-  
+  session[RESPONSES_KEY] = responses
+
+
   if (responses is None):
         return redirect("/")
 
@@ -40,6 +50,8 @@ def answer():
         return redirect("/complete")
 
   return redirect(f"/questions/{len(responses)}")
+
+#----------------------------------------------------------------------------------------------
 
 @app.route('/complete', methods=['GET', 'POST'])
 def complete_page():
